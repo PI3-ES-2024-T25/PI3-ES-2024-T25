87e34d0b2153aa0be9615944d46cbe9bda19c04c
CaioCSGandara <caiocsgandara@gmail.com>
2024-03-25 19:23:40 -0300

    chore: finalização do layout com guidelines, criação da classe de login

---
+++ app/src/main/java/br/edu/puccampinas/pi3_es_2024_time_25/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.puccampinas.pi3_es_2024_time_25
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
+// até esse ponto, a classe apenas serve para "vestir" o layout. nao possui nenhuma funcionalidade
