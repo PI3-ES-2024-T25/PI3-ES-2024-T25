f58cf8e5163cb2f07769a40c42e608331d950399
CaioCSGandara <caiocsgandara@gmail.com>
2024-04-19 17:24:08 -0300

    chore: ajustes da tela de adicionar cartão

---
+++ app/src/main/java/br/edu/puccampinas/pi3_es_2024_time_25/CreditCard.kt
@@ -0,0 +1,13 @@
+package br.edu.puccampinas.pi3_es_2024_time_25
+
+class CreditCard(
+                var cid: String,
+                var titular: String,
+                var cardNumber: Int,
+                var cardExpiration: String,
+                var cvv: Int) {
+
+    class Validator {
+
+    }
+}
