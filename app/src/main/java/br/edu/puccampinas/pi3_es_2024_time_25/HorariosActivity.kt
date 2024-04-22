e2a40d623a181d150b74b652fe2933ef53b77a1e
Vinicius Kenji Okita Eugenio <vinicius.koe@puccampinas.edu.br>
2024-04-21 23:23:44 -0300

    fix: ajustes tela horarios

--- app/src/main/java/br/edu/puccampinas/pi3_es_2024_time_25/HorariosActivity.kt
+++ app/src/main/java/br/edu/puccampinas/pi3_es_2024_time_25/HorariosActivity.kt
@@ -1,13 +1,11 @@
 package br.edu.puccampinas.pi3_es_2024_time_25
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.toColor
 import com.google.android.material.snackbar.Snackbar
 
 class HorariosActivity : AppCompatActivity() {
@@ -20,6 +18,8 @@ class HorariosActivity : AppCompatActivity() {
     private lateinit var btnTempo5: Button
     private lateinit var confirmarLocacao: Button
     private lateinit var voltarTempo: Button
+
+    private var selectedButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,69 +40,43 @@ class HorariosActivity : AppCompatActivity() {
 
         val buttons = listOf(btnTempo1, btnTempo2, btnTempo3, btnTempo4, btnTempo5)
 
-        confirmarLocacao.setOnClickListener {
-            val horarioSelecionado = buttons.any { button ->
-                button.currentTextColor == ContextCompat.getColor(this, R.color.green)
-            }
-
-            if (horarioSelecionado) {
-                startActivity(Intent(this, Register2Activity::class.java))
-                finish()
-            } else {
-                val msg = "Por favor, selecione um horário de locação."
-                Snackbar.make(it, msg, Snackbar.LENGTH_SHORT).show()
-            }
-        }
-
-
         buttons.forEach { button ->
             button.setOnClickListener {
-                textoHorarios.text = "Horário selecionado: ${button.text}"
-                button.setBackgroundColor(
-                    ContextCompat.getColor(
-                        this,
-                        R.color.white
-                    )
-                )
-                confirmarLocacao.isEnabled = true
-                confirmarLocacao.setBackgroundColor(
-                    ContextCompat.getColor(
-                        this,
-                        R.color.green
-                    )
-                )
+                if (selectedButton == button) {
+
+                    button.setBackgroundResource(R.drawable.primary_background_btn)
+                    selectedButton = null
+                    textoHorarios.text = "Nenhum horário selecionado"
+
+
+                    confirmarLocacao.isEnabled = false
+                    confirmarLocacao.setBackgroundResource(R.drawable.unselected_button)
+                } else {
+
+                    selectedButton?.setBackgroundResource(R.color.white)
+                    selectedButton = button
+
+
+                    button.setBackgroundResource(R.drawable.selected_button)
+                    textoHorarios.text = "Horário selecionado: ${button.text}"
+
+
+                    confirmarLocacao.isEnabled = true
+                    confirmarLocacao.setBackgroundResource(R.drawable.selected_button)
+                }
             }
         }
 
         confirmarLocacao.setOnClickListener {
-            if (confirmarLocacao.isEnabled) {
-                buttons.forEach { btn ->
-                    if (btn.currentTextColor == ContextCompat.getColor(this, R.color.green_dark)) {
-                        btn.setBackgroundColor(
-                            ContextCompat.getColor(
-                                this,
-                                R.color.green
-                            )
-                        ) // Cor quando confirmado
-                    }
-                }
-                confirmarLocacao.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+            if (!confirmarLocacao.isEnabled) {
+                val msg = "Por favor, selecione um horário de locação."
+                Snackbar.make(it, msg, Snackbar.LENGTH_SHORT).show()
+            } else {
+
+                startActivity(Intent(this, Register2Activity::class.java))
+                finish()
             }
-        }
-
-
-        fun resetButtonColors() {
-
-            val timeButtons = listOf(btnTempo1, btnTempo2, btnTempo3, btnTempo4, btnTempo5)
-
-
-            timeButtons.forEach { button ->
-                button.setBackgroundResource(R.color.white)
-            }
-
-
-            confirmarLocacao.setBackgroundColor(ContextCompat.getColor(this, R.color.green_dark))
-            confirmarLocacao.isEnabled = false
         }
     }
 }
+
